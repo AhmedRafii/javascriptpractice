cfa4238d20ce4363d30ce0f01eebcb3c4faf81f6
Desperate <capsicumlb@gmail.com>
2023-11-11 21:12:47 +0600

    javascript datatype

---
+++ datatype.js
@@ -0,0 +1,30 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score))
+ 
+let valueInNumber = Number(score) //to convert the string to the number
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// if value is 33 then it is a number
+// if value is "33" then it is a string
+// if value is rafi then it is a string
+// if value is true/false then it is boolen
+
+
+let isLoggedIn = 1
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn)
+
+// In boolean if value is 1 then it will be true
+// In boolean if value is 0 then it will be fasle
+// In boolean if there is no value then it will be false
+// In boolean if there is something like "rafi" it will be true
+
+let someNumber = 77
+let stringNumber= String(someNumber)
+console.log(typeof stringNumber);
+
+// we can anytime convert the datatypes.
